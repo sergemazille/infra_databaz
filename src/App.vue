6d0136bc7adc72b3f30e092774e6b78e6c4e1f09
Serge Mazille <serge.mazille@gmail.com>
2020-11-30 06:51:42 +0100

    Move init from App to main.ts

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,3 @@
   </div>
   <router-view />
 </template>
-
-<script>
-import { fetchConfigs } from '@/utils/localstorage.ts';
-
-export default {
-  async created() {
-    const configs = await fetchConfigs();
-    this.$store.dispatch('setConfigs', configs);
-  },
-};
-</script>
-
-<style lang="scss">
-@import 'style/reset.css';
-@import 'style/common.scss';
-</style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// global style
+import './style/reset.css';
+import './style/common.scss';
+
+// initialize configs
+store.dispatch('retrieveConfigs');
+
 createApp(App)
   .use(store)
   .use(router)
