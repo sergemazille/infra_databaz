2b8b191c6c600e4f09131bb2f53d62895bd24871
Serge Mazille <serge.mazille@gmail.com>
2020-12-01 07:52:38 +0100

    Add ssh connection method

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,9 +1,13 @@
+import { Config } from '@/domain/Config.ts';
+
 const os = window.require('os');
 const {
   remote,
   remote: { app },
 } = window.require('electron');
 const path = window.require('path');
+const { NodeSSH } = window.require('node-ssh');
+const ssh = new NodeSSH();
 
 export const getUsername = () => {
   return os.userInfo().username;
@@ -21,4 +25,25 @@ export const browseForSshPrivateKeyPath = () => {
   });
 
   return keyPath;
+};
+
+const connect = (config: Config) => {
+  const { serverIp, serverSshPort, serverUsername, serverPassword, sshPrivateKeyPath } = config;
+
+  return new Promise((resolve, reject) => {
+    ssh
+      .connect({
+        host: serverIp,
+        port: serverSshPort,
+        username: serverUsername,
+        password: serverPassword,
+        privateKey: sshPrivateKeyPath,
+      })
+      .then(() => {
+        return resolve(ssh);
+      })
+      .catch((error: any) => {
+        return reject(error);
+      });
+  });
 };
