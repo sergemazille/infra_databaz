32cae7df951888d0513a189d919456d8f4e46a34
Serge Mazille <serge.mazille@gmail.com>
2020-12-02 07:26:38 +0100

    Add save db feature

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -3,22 +3,19 @@ import store from '@/store/index.ts';
 import { Type as NotificationType } from '@/domain/Notification.ts';
 import { formatedNow } from '@/utils/date.ts';
 
-const os = window.require('os');
 const {
-  remote,
-  remote: { app },
+  remote: { app, dialog },
 } = window.require('electron');
 const path = window.require('path');
 const { NodeSSH } = window.require('node-ssh');
 const ssh = new NodeSSH();
 
-export const getUsername = () => {
-  return os.userInfo().username;
+const getUserHomePath = () => {
+  return app.getPath('home');
 };
 
 export const browseForSshPrivateKeyPath = () => {
-  const dialog = remote.dialog;
-  const home = app.getPath('home');
+  const home = getUserHomePath();
   const defaultKeyPath = path.join(home, '.ssh');
 
   const [keyPath] = dialog.showOpenDialogSync({
@@ -28,6 +25,48 @@ export const browseForSshPrivateKeyPath = () => {
   });
 
   return keyPath;
+};
+
+const generateSqlFileNameFromDate = () => {
+  const now = formatedNow();
+  return `${now}.sql`;
+};
+
+const importDbFile = (remoteDumpPath: string) => {
+  const defaultFileName = generateSqlFileNameFromDate();
+  const userHomePath = getUserHomePath();
+
+  return new Promise((resolve, reject) => {
+    dialog
+      .showSaveDialog({
+        title: 'Choisir où sauvegarder la base de données',
+        defaultPath: path.join(userHomePath, `/${defaultFileName}`),
+        buttonLabel: 'Enregistrer',
+        filters: [
+          {
+            name: 'Fichiers SQL',
+            extensions: ['sql'],
+          },
+        ],
+      })
+      .then((file: any) => {
+        if (file.canceled) {
+          return resolve();
+        }
+
+        // copy file
+        ssh.getFile(file.filePath.toString(), `${remoteDumpPath}`);
+        return resolve();
+      })
+      .catch((error: any) => {
+        store.dispatch('setNotification', {
+          message: `Problème lors de la récupération de la sauvegarde : ${error}`,
+          type: NotificationType.ERROR,
+        });
+
+        return reject();
+      });
+  });
 };
 
 const connect = (config: Config) => {
@@ -46,41 +85,50 @@ const connect = (config: Config) => {
         return resolve(ssh);
       })
       .catch((error: any) => {
-        return reject(error);
+        store.dispatch('setNotification', {
+          message: `Problème de connexion à la base de données : ${error}`,
+          type: NotificationType.ERROR,
+        });
+
+        return reject();
       });
   });
 };
 
 export const saveDb = (config: Config) => {
   const { dbName, dbUsername, dbPassword } = config;
+  const dumpName = generateSqlFileNameFromDate();
+  const remoteTempPath = app.getPath('temp');
+  const remoteDumpPath = `${remoteTempPath}/${dumpName}`;
 
   connect(config)
     .then(() => {
-      const tempPath = app.getPath('temp');
-      const dumpName = `${formatedNow()}.sql`;
-      const dumpCommand = `mysqldump -u ${dbUsername} -p${dbPassword} ${dbName} > ${tempPath}/${dumpName}`;
+      // create remote dump
+      const dumpCommand = `mysqldump -u ${dbUsername} -p${dbPassword} ${dbName} > ${remoteDumpPath}`;
 
       return ssh.execCommand(dumpCommand);
     })
     .then(() => {
-      // saveFile();
+      // save local file
+      return importDbFile(remoteDumpPath);
     })
     .then(() => {
-      // remove dump;
+      // remove remote dump
+      const dumpCommand = `rm ${remoteDumpPath}`;
+
+      return ssh.execCommand(dumpCommand);
     })
     .then(() => {
-      const notification = {
+      store.dispatch('setNotification', {
         message: 'Opération terminée avec succès',
         type: NotificationType.SUCCESS,
-      };
-      store.dispatch('setNotification', notification);
+      });
     })
     .catch(error => {
-      const notification = {
-        message: `Problème de connexion à la base de données : ${error}`,
+      store.dispatch('setNotification', {
+        message: `Problème lors de l'opération : ${error}`,
         type: NotificationType.ERROR,
-      };
-      store.dispatch('setNotification', notification);
+      });
     });
 };
 
@@ -90,10 +138,9 @@ export const restoreDb = (config: Config) => {
       console.log('=== ssh.isConnected() ===>', ssh.isConnected());
     })
     .catch(error => {
-      const notification = {
+      store.dispatch('setNotification', {
         message: `Problème de connexion à la base de données : ${error}`,
         type: NotificationType.ERROR,
-      };
-      store.dispatch('setNotification', notification);
+      });
     });
 };
